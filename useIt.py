09379176effd0ddd9a9e700ba133a3dd935775d1
Tim Kleinschmidt <tim.kleinschmidt@googlemail.com>
2012-02-20 20:40:55 +0100

    Some cleanups

--- useIt.py
+++ useIt.py
@@ -11,17 +11,11 @@ class useItCommand(sublime_plugin.TextCommand):
     """
 
     def run(self, edit):
-        if len(self.view.file_name()) > 0:
-            word = self.view.substr(self.view.word(self.view.sel()[0].begin()))
-            for region in self.view.sel():
-                phrase = self.view.substr(region)
-                search = 'http://caniuse.com/#search='
-                if not region.empty():
-                    webbrowser.open_new_tab(search + phrase)
-                else:
-                    webbrowser.open_new_tab(search + word)
-        else:
-            pass
-
-    def is_enabled(self):
-        return self.view.file_name() and len(self.view.file_name()) > 0
+        word = self.view.substr(self.view.word(self.view.sel()[0].begin()))
+        for region in self.view.sel():
+            phrase = self.view.substr(region)
+            search = 'http://caniuse.com/#search='
+            if not region.empty():
+                webbrowser.open_new_tab(search + phrase)
+            else:
+                webbrowser.open_new_tab(search + word)
